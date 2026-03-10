236cf5dec726dff042578533581794008e85f396
Jonathan Strong <142829586+thejoeman23@users.noreply.github.com>
2026-03-09 22:46:22 -0400

    Added safari support

---
+++ No Algorithms/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Jonathan Strong on 2026-03-09.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
