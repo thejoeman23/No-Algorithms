f446850292151a141dac486548b4dd8924f457f6
Jonathan Strong <142829586+thejoeman23@users.noreply.github.com>
2026-07-24 21:41:48 -0400

    Quick Ammend

--- Base Extension/src/feature.ts
+++ Base Extension/src/feature.ts
@@ -18,6 +18,7 @@ interface NoAlgorithmsFeature {
 interface NoAlgorithmsFeatureMetadata {
   enabled?: boolean;
   schedule?: FeatureSchedule;
+  scheduleEnabled?: boolean;
   enabledUntil?: string;
 }
 
